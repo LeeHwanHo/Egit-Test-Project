0f5471737ff32341ced5c2bc2414f8eb12380c6f
LeeHwanHo <ghksgh45@gmail.com>
2019-12-16 16:05:16 +0900

    두번째 커밋

--- EgitTest/src/Main.java
+++ EgitTest/src/Main.java
@@ -1,9 +1,10 @@
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is egit");
+		
+		Test test = new Test();
 	}
 
 }
---
+++ EgitTest/src/Test.java
@@ -0,0 +1,9 @@
+
+class Test {
+	
+	String comment = "This is Test Class Object!";
+	
+	public Test(){
+		System.out.println(comment);
+	}
+}
