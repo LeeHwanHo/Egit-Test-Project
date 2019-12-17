572df1a91a266c0d58630c01a66930f29e1af2e6
LeeHwanHo <ghksgh45@gmail.com>
2019-12-17 09:04:06 +0900

    브랜치 추가 테스트 commit

--- EgitTest/src/Test.java
+++ EgitTest/src/Test.java
@@ -3,8 +3,17 @@ class Test {
 	
 	String comment = "This is Test Class Object!";
 	
+	
+	//hotfix branch 에서의 코드 추가
+	int num = 10;
+	
 	public Test(){
 		System.out.println(comment);
+	}
+	
+	//hotfix branch 에서의 코드 추가
+	public Test(int num) {
+		this.num = num;
 	}
 	
 	
